263291615efee9302182ebcf2aa0de2bc2be612b
Harshil Sharma <harshilsharma.dev@gmail.com>
2025-11-27 23:02:27 +0530

    fix error

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import globe from '../assets/globe.png'
 export default function Header() {
     return (
         <header>
-            <img id="logo" src={globe}></img>
+            <img id="logo" src={globe} alt="Globe icon" />
             <h1 id="title">my travel journal.</h1>
         </header>
     )
